f78afa412fb1c807430e989b72e988f79fbf3940
Steve Lyster <steven.lyster@gmail.com>
2020-08-27 08:55:23 -0600

    Working

--- HG_EDA_SL.R
+++ HG_EDA_SL.R
@@ -2,6 +2,7 @@
 
 library("tidyverse")
 
+# Load the data
 game_data <- read_csv("https://raw.githubusercontent.com/hockey-graphs/hg-data-cleaning/master/game_data.csv")
 
 view(game_data)
@@ -13,6 +14,7 @@ game_data %>%
   group_by(game_id,event_type) %>%
   summarize(n())
 
+# Get the powerplay data
 PP <- game_data %>%
   filter(game_strength_state %in% c("5v4","4v5") & (event_length > 0 | event_type %in% c("SHOT","GOAL","MISS","BLOCK")))
 
@@ -20,6 +22,7 @@ PP %>%
   group_by(game_id,event_type) %>%
   summarize(n())
 
+# Pivot the data
 PP_on_ice <- PP %>%
   pivot_longer(cols=home_on_1:away_on_7, names_to="on_ice", values_to="player") %>%
   mutate(team = ifelse(str_detect(on_ice,"home"),home_team,away_team))
